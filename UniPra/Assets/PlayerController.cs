c6105dc20eaa35f200c570560c54c54708d54801
CP5S25 <yokohama.vcp@gmail.com>
2018-11-09 15:24:54 +0900

    This is Original Project.

---
+++ UniPra/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// プレイヤーの移動(操作)をここにまとめる
+public class PlayerController : MonoBehaviour {
+
+    public GameObject chaseObject;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float chasedirx = this.transform.position.x - this.chaseObject.transform.position.x;
+        float chasediry = this.transform.position.y - this.chaseObject.transform.position.y;
+
+        float chasedirlength = Mathf.Sqrt(chasedirx * chasedirx + chasediry * chasediry);
+
+        transform.Translate(chasedirx / chasedirlength / 10, chasediry / chasedirlength / 10, 0);
+        
+        //transform.position.x += chasedirx / chasedirlength;
+    }
+}
